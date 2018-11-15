c2f69eaf029b043b880c3930173eb8ca50b8331e
va@no.where.com <va@no.where.com>
2018-11-15 17:03:05 +0200

    init

--- lib/issues.ex
+++ lib/issues.ex
@@ -12,14 +12,20 @@ defmodule Issues do
       :world
 
   """
-  def get() do
+  def get_page(cursor) do
     token = System.get_env("GH_TOKEN")
+    pagination = ""
+    if cursor do
+      pagination = """
+        ,after: \\"#{cursor}\\"
+        """
+    end
     org = "octocat"
     repo = "Hello-World"
     query = """
     { "query": "query {
       repository(owner:\\"#{org}\\", name:\\"#{repo}\\") {
-        issues(last:20, states:CLOSED) {
+        issues(first:20 #{pagination}, states:CLOSED) {
           edges {
             node {
               title url labels(first:5) {
@@ -45,12 +51,27 @@ defmodule Issues do
                             [{"Content-Type", "application/json"},
                             {"Authorization", "Bearer #{token}"}])
     {status_code, body} = { Map.get(resp, :status_code), Map.get(resp, :body) }
+  end
+
+  def get(cursor, pagenum \\ 0) do
+    if pagenum > 5 do
+      Process.exit(self, :normal)
+    end
+    {status_code, body} = get_page(cursor)
     case {status_code, body} do
       {200, _} ->
-        IO.inspect(body)
+        Poison.Parser.parse!(body)
+        |> Map.get("data")
+        |> Map.get("repository")
+        |> Map.get("issues")
+        |> Map.get("edges")
+        |> Enum.at(-1)
+        |> Map.get("cursor")
+        |> IO.inspect
+        |> get(pagenum + 1)
         :ok
       _ ->
-        IO.inspect(resp)
+        IO.inspect(status_code)
         IO.inspect("[ERROR]")
         :error
     end
