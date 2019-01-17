f2d84ef67f1d99e35920e92638f7b0807b708eee
va@no.where.com <va@no.where.com>
2019-01-17 12:08:34 +0200

    elixir 1.7

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Issues.MixProject do
       app: :issues,
       escript: escript_config(),
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
